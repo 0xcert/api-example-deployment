70e2179e8f8f1f7207b3de8893a73ccd329fc3ce
Tadej Vengust <vengust.tadej@gmail.com>
2020-03-02 13:20:29 +0100

    Bump dependencies and api url.

--- src/example.ts
+++ src/example.ts
@@ -20,7 +20,7 @@ export async function init() {
 
   config.client = new Client({
     provider,
-    apiUrl: "https://api-staging.0xcert.org"
+    apiUrl: "https://api-rinkeby.0xcert.org"
   });
 
   return config.client.init();
